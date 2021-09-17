4ace7ca7209b3f7d56a962300cd3894cd34da146
Mehdi <mehdi@mehdi-aberkane.fr>
2021-09-17 16:46:42 +0200

    some stuff

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
+var listing = document.querySelector("#listing")
+var number_manga = 0
+
+function build_list() {
+    Object.keys(localStorage).forEach(function(key) {
+        if (key.includes('manga')) {
+            const new_tag = document.createElement("li")
+            new_tag.innerHTML = localStorage.getItem(key)
+            new_tag.classList = "list-group-item active"
+            
+            listing.appendChild(new_tag)
+            number_manga++
+        }
+    });
+}
+
+build_list()
+
 function add_new_manga() {
-    const manga = document.querySelector("#manga");
+    const manga = document.querySelector("#manga")
     const i = parseInt(localStorage.length) + 1
 
     localStorage.setItem('manga_' + i, manga.value)
     manga.value = ""
+    build_list()
 }
 
 function reset() {
     localStorage.clear()
 }
-localStorage.setItem('qsdsqdsqd' , "dddd.value")
 
-const add_manga = document.querySelector("#add_manga");
-add_manga.addEventListener("click", add_new_manga, false);
-
-Object.keys(localStorage).forEach(function(key){
-    if (key.includes('manga')) {
-        console.log(localStorage.getItem(key))
-    }
- });
-
+const add_manga = document.querySelector("#add_manga")
+add_manga.addEventListener("click", add_new_manga, false)
